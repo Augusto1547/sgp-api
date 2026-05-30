f6c59c5de96b5b28fa76426e59d93443729146c4
Agusto César <negocios.augusto@gmail.com>
2026-05-30 15:05:36 -0300

    Cria os diretórios Service, Contreller e Model e seus respectivos arquivos mais o o primeiro insert no banco pelo postman.

--- src/main/java/br/com/treina/recife/sgp/api/model/Tarefa.java
+++ src/main/java/br/com/treina/recife/sgp/api/model/Tarefa.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import br.com.treina.recife.sgp.api.enums.Prioridade;
 import br.com.treina.recife.sgp.api.enums.StatusTarefa;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -12,7 +13,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "TB_TAREFAS")
 public class Tarefa {
 
     @Id
