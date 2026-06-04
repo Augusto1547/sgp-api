d23e03842818b84288547fc7b879c92286060553
Agusto César <negocios.augusto@gmail.com>
2026-06-04 17:12:10 -0300

    Remover a anotação: @Date por @Getter e @Setter

--- src/main/java/br/com/treina/recife/sgp/api/model/Usuario.java
+++ src/main/java/br/com/treina/recife/sgp/api/model/Usuario.java
@@ -10,14 +10,18 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Entity
+@Table(name = "TB_USUARIOS")
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "TB_USUARIOS")
 public class Usuario {
 
   @Id
